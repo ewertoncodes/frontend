05cbcfbba5d7dac684b6b1a14791bffed6b45d26
Ewerton Nascimento <ewertonorg@gmail.com>
2020-05-03 19:40:36 -0300

    Importando CSS e imagens

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
+import './App.css';
+import backgroundImage from './assets/background.jpeg';
+
 import Header from './components/Header';
 
 function App(){
   const [projects, setProject] = useState(['Desenvolvimento de app', 'Frontend web']);
 
   function handleAddProject(){
-    //projects.push(`Novo projeto ${Date.now()}`);
     setProject([...projects, `Novo projeto ${Date.now()}`]);
-    console.log(projects)
   };
 
   return(
     <>
       <Header title='Projects'/>
+      <img width={300} src={backgroundImage}/>
       <ul>
         { projects.map(project => <li key = {project}> {project} </li>) }
       </ul>
